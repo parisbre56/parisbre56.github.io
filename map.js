70599e0585945b4634aab293e5c79ddcd65325d2
parisbre56 <parisbre56@users.noreply.github.com>
2018-09-23 01:44:44 +0300

    rgb to hex

--- map.js
+++ map.js
@@ -367,6 +367,12 @@ function getColorAtPos(tPosX, tPosY) {
 	return hex;
 }
 
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
 function pointerStore(e) {
 	storedGridX = posXToGridX(mouseX);
 	storedGridY = posYToGridY(mouseY);
